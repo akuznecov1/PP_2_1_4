01cb2a73e6e1a5a7c39dffc1368c646116d9e402
[akuznecov1] <kuznecov0996@gmail.com>
2022-10-01 13:54:49 +0500

    KoscheiDEAD

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    @Autowired
-    private Egg6 egg;
+
+    private final Egg6 egg;
+
+    public Duck5(Egg6 egg){
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
+    private Needle7 needle;
 
-    @Autowired
-    private Needle7 needle7;
+    public Egg6(Needle7 needle){
+        this.needle = needle;
+
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle7.toString();
+        return ", в яйце иголка " + needle.toString();
     }
 }
